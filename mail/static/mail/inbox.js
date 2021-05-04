7cb0e294c69f621de6a9e840a69c130a92c65f1c
Jamie <bagginsbilbo312@gmail.com>
2021-05-04 16:14:27 +0100

    can now send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,21 +5,47 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#test').addEventListener('click', send_email);
+
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
 
+function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  document.querySelector('#compose-form').onsubmit = send_email;
+  
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  console.log("Compose_email")
+
+}
+
+function send_email() {
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value,
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log("Email Sent" + document.querySelector('#compose-body').value);
+      document.getElementById("p1").innerHTML = "New text!";
+      load_mailbox('sent');
+  });
+  return false;
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +56,14 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
+
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      //console.log(emails);
+
+      // ... do something else with emails ...
+  });
+
+  }
